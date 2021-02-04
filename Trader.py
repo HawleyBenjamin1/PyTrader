5b38f99fa5ced1fa6491bfee803fed619ba1e2e8
Ben <hawleybenjamin1@gmail.com>
2021-02-04 14:25:28 -0500

    Implemented logic to sell positions from current portfolio.

--- Trader.py
+++ Trader.py
@@ -12,12 +12,13 @@ class Trader:
         with open(config_file_name) as f:
             self.CONFIG = json.load(f)
         self.HEADERS = {'APCA-API-KEY-ID': self.CONFIG["api_key_id"], "APCA-API-SECRET-KEY": self.CONFIG["secret_key"]}
+        self.PAPER_HEADERS = {'APCA-API-KEY-ID': self.CONFIG["paper_api_key_id"], "APCA-API-SECRET-KEY": self.CONFIG["paper_secret_key"]}
         self.BASE_PAPER_URL = self.CONFIG["base_paper_url"]
         self.BASE_URL = self.CONFIG["base_url"]
         self.BASE_MKT_DATA_URL = self.CONFIG["base_mkt_data_url"]
 
     def get_account(self):
-        return requests.get(self.BASE_URL + self.CONFIG["account_endpoint"], headers=self.HEADERS)
+        return requests.get(self.BASE_URL + self.CONFIG["account_endpoint"], headers=self.PAPER_HEADERS)
 
     def create_order(self, symbol, qty, side, order_type, time_in_force):
         DATA = {
@@ -27,13 +28,13 @@ class Trader:
             "type": order_type,
             "time_in_force": time_in_force
         }
-        return requests.post(self.BASE_URL + self.CONFIG["orders_endpoint"], json=DATA, headers=self.HEADERS)
+        return requests.post(self.BASE_PAPER_URL + self.CONFIG["orders_endpoint"], json=DATA, headers=self.PAPER_HEADERS)
 
     def get_positions(self):
-        return requests.get(self.BASE_URL + self.CONFIG["positions_endpoint"], headers=self.HEADERS)
+        return requests.get(self.BASE_PAPER_URL + self.CONFIG["positions_endpoint"], headers=self.PAPER_HEADERS)
 
     def get_assets(self):
-        return requests.get(self.BASE_URL + self.CONFIG["assets_endpoint"], headers=self.HEADERS)
+        return requests.get(self.BASE_PAPER_URL + self.CONFIG["assets_endpoint"], headers=self.PAPER_HEADERS)
 
     # No more than 200 symbols allowed
     # Limit: max number of bars for each symbol between 1 and 1000, default: 100
@@ -72,7 +73,7 @@ def get_bar_data(tickers, max_bars, after_date="2019-01-01"):
 
         conn = sqlalchemy.create_engine(config["db_connection_string"])
 
-        df.to_sql(ticker, schema="bar_data", con=conn, if_exists="append")
+        df.to_sql(ticker, schema="bar_data", con=conn, if_exists="replace")
 
 
 def update_positions():
@@ -85,19 +86,34 @@ def update_positions():
     return df
 
 
+def sellable(unrealized_plpc):
+    if unrealized_plpc > .05 or unrealized_plpc < -.05:
+        return True
+    else:
+        return False
+
+
 # TODO: Create trading strategy
 def main():
     config = json.load(open("config.json"))
-    # positions = update_positions()
-    # get_bar_data(tickers=["TWTR", "AAPL", "AMZN", "TSLA"], max_bars=1000)
+    positions = update_positions()
+    # get_bar_data(tickers=["TWTR"], max_bars=1000)
 
     trader = Trader("config.json")
     account_dict = trader.get_account().json()
 
-    # TODO: Check for positions to sell
     conn = sqlalchemy.create_engine(config["db_connection_string"])
     twtr = pd.read_sql_table(con=conn, table_name="TWTR", schema="bar_data", index_col="index")
-    breakpoint()
+
+    # TODO: Check for positions to sell
+    sell_positions = positions.apply(lambda x:
+                                     [x["symbol"], x["qty"], "sell"]
+                                     if sellable(x["unrealized_plpc"]) else [x["symbol"], x["qty"], "hold"], axis=1)
+
+    order_response = sell_positions.apply(lambda x:
+                                          trader.create_order(x[0], x[1], "sell", "mkt", "gtc").text
+                                          if x[2] is "sell" else x[0] + ": hold")
+    print(order_response)
 
     # TODO: Check for positions to buy
 
